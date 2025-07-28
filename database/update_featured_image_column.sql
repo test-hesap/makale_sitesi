f909eeff477f63393f7d264711085c0a78ae1880
the_joker <terkiterki13@gmail.com>
2025-07-28 23:41:07 +0300

    Delete database/update_featured_image_column.sql

--- database/update_featured_image_column.sql
+++
@@ -1,5 +0,0 @@
--- featured_image ve cover_image_url sütunlarını TEXT olarak değiştir
-ALTER TABLE `articles` MODIFY COLUMN `featured_image` TEXT DEFAULT NULL;
-ALTER TABLE `ai_articles` MODIFY COLUMN `cover_image_url` TEXT DEFAULT NULL;
-ALTER TABLE `ai_articles` MODIFY COLUMN `image1_url` TEXT DEFAULT NULL;
-ALTER TABLE `ai_articles` MODIFY COLUMN `image2_url` TEXT DEFAULT NULL;
